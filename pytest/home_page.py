4a54958768174d7915f6bb04f3a952c707c34b71
davidpape91 <david_parra@epam.com>
2025-05-14 13:40:31 -0500

    Implemented more assertions and related functions

--- pytest/home_page.py
+++ pytest/home_page.py
@@ -11,7 +11,7 @@ class HomePage:
     def navigate_to_homepage(self):
         self.driver.get("https://www.demoblaze.com/")
 
-    def login(self, username, password):
+    def click_login_link(self):
         WebDriverWait(self.driver, 10).until(
             ec.element_to_be_clickable(Locators.LOGIN_LINK)
         ).click()
@@ -20,6 +20,13 @@ class HomePage:
             ec.visibility_of_element_located(Locators.MODAL)
         )
 
+    def login_fields_presence(self):
+        username = self.driver.find_element(*Locators.USERNAME_INPUT)
+        password = self.driver.find_element(*Locators.PASSWORD_INPUT)
+
+        return username.is_displayed() and password.is_displayed() and username.is_enabled() and password.is_enabled()
+
+    def submit_login_form(self, username, password):
         self.driver.find_element(*Locators.USERNAME_INPUT).send_keys(username)
         self.driver.find_element(*Locators.PASSWORD_INPUT).send_keys(password)
         self.driver.find_element(*Locators.LOGIN_BUTTON).click()
@@ -34,10 +41,58 @@ class HomePage:
 
     def navigate_to_monitors_page(self):
         WebDriverWait(self.driver, 10).until(
-            ec.element_to_be_clickable(Locators.MONITOR_LINK)
+            ec.element_to_be_clickable(Locators.MONITORS_LINK)
         ).click()
 
+        sleep(2)
+
     def click_highest_price_monitor(self):
-        monitors = WebDriverWait(self.driver, 10).until(
+        monitors_row = WebDriverWait(self.driver, 10).until(
             ec.presence_of_element_located(Locators.MONITOR_ROW)
         )
+
+        monitors = WebDriverWait(monitors_row, 10).until(
+            ec.presence_of_all_elements_located(Locators.MONITOR_CARD)
+        )
+
+        product_name = ""
+        highest_price = 0
+        expensive_monitor = ""
+
+        for monitor in monitors:
+            price = monitor.find_element(*Locators.MONITOR_PRICE).text
+            price = int(price.replace("$", ""))
+
+            if price > highest_price:
+                highest_price = price
+                expensive_monitor = monitor
+                product_name = monitor.find_element(*Locators.MONITOR_LINK).text
+
+        expensive_monitor.click()
+
+        return product_name, highest_price
+
+    def add_monitor_to_cart(self):
+        WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located(Locators.CART_BUTTON)
+        ).click()
+
+        sleep(2)
+
+        alert = self.driver.switch_to.alert
+        alert.accept()
+
+    def navigate_to_cart(self):
+        WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located(Locators.CART_LINK)
+        ).click()
+
+        cart_entry = WebDriverWait(self.driver, 10).until(
+            ec.presence_of_element_located(Locators.CART_ENTRY)
+        )
+
+        tds = cart_entry.find_elements(*Locators.TDS)
+
+        product_name, product_price = tds[1].text, int(tds[2].text)
+
+        return product_name, product_price
--- pytest/tests.py
+++ pytest/tests.py
@@ -2,6 +2,7 @@ import pytest
 from home_page import HomePage
 from selenium import webdriver
 
+
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome()
@@ -13,15 +14,27 @@ def test_login(driver, username, password):
     page = HomePage(driver)
 
     page.navigate_to_homepage() # navigates to demoblaze website
-    page.login(username, password) # logs into the website
+    page.click_login_link()  # clicks on login link
 
-    assert page.check_login_message(username) == True
+    assert page.login_fields_presence() == True # checks that both fields are present
 
-@pytest.mark.parametrize("username, password", [("someuser2", "afkpc123")])
-def test_shopping_cart(driver, username, password):
+    page.submit_login_form(username, password)  # logs into the website
+
+    assert page.check_login_message(username) == True # asserts that we got the welcome message
+
+@pytest.mark.parametrize("username, password, product_name, product_price", [("someuser2", "afkpc123", "Apple monitor 24", 400)])
+def test_shopping_cart(driver, username, password, product_name, product_price):
     page = HomePage(driver)
 
     page.navigate_to_homepage()  # navigates to demoblaze website
-    page.login(username, password)  # logs into the website
+    page.click_login_link() # clicks on login link
+    page.submit_login_form(username, password)  # logs into the website
     page.navigate_to_monitors_page() # navigates to monitors page
-    page.click_highest_price_monitor() # selects on the monitor with the highest price
+    name, price = page.click_highest_price_monitor() # selects on the monitor with the highest price
+
+    assert name == product_name and price == product_price # here im supposed to assert that we got to the right page
+
+    page.add_monitor_to_cart() # adds the monitor with the highest price to the cart
+    name, price = page.navigate_to_cart() # navigates to cart page
+
+    assert name == product_name and price == product_price # here im supposed to assert that the monitor was added to the cart
